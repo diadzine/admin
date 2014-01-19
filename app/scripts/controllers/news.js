f7ea93760d49f2fb2b542411a62cce41900cdfeb
seba-1511 <seba-1511@hotmail.com>
2014-01-20 00:52:49 +0100

    More changes to the news page.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -1,7 +1,33 @@
 'use strict';
 
 angular.module('adminApp')
-    .controller('NewsCtrl', function($scope) {
+    .controller('NewsCtrl', function($scope, News) {
+        var news = News.getNews();
+
+
+        $scope.news = news;
+
+        $scope.sendNews = function() {
+            alert('News Sent');
+            // should check date, title, tinymce
+            // if id = -1 create new, else, modify
+        };
+
+        $scope.addNews = function() {
+            // Should reset tinymce, the date, title and id field. change text of button.
+        };
+
+        $scope.modify = function(newsId) {
+            // TODO: implement change content of tinymce. (not working yet.)
+            var news = News.getNews(newsId);
+            document.getElementById('newsSend').innerHTML =
+                'Envoyer modifications';
+            document.getElementById('newsTitle').value = news.title;
+            document.getElementById('tinymce').innerHTML = news.content;
+            document.getElementById('newsId').value = news.id;
+            $scope.tinyMceContent = news.content;
+        };
+
         $scope.tinymceOptions = {
             selector: "textarea",
             theme: "modern",
--- app/scripts/services/news.js
+++ app/scripts/services/news.js
@@ -1,16 +1,82 @@
 'use strict';
 
 angular.module('adminApp')
-  .factory('News', function () {
-    // Service logic
-    // ...
+    .factory('News', function() {
+        var news = [{
+            id: 0,
+            author: 'Séb',
+            title: 'News n°0 avec super long titre',
+            content: 'Mon super text, avec du html: <img src="http://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" /><a href="http://tooski.ch"></a>',
+            date: 1389390694,
+            mag: 1,
+        }, {
+            id: 1,
+            author: 'Séb',
+            title: 'News n°1',
+            content: 'Mon super text, avec du html: <img src="http://www.fricktal24.ch/typo3temp/pics/Ski-Aerni_Luca_01_55d138efee.jpg" /><a href="http://tooski.ch"></a>',
+            date: 1389390695,
+            mag: 1,
+        }, {
+            id: 2,
+            author: 'Séb',
+            title: 'News n°2',
+            content: 'Mon super text, avec du html: <a href="https://tooski.ch"><img src="http://skiweltcup.tv/wp-content/themes/tvsportnews/images/09-aerni002-klein-swiss-ski.jpg" /></a>',
+            date: 1389390696,
+            mag: 1,
+        }, {
+            id: 0,
+            author: 'Séb',
+            title: 'News n°0',
+            content: 'Mon super text, avec du html: <img src="http://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" /><a href="http://tooski.ch"></a>',
+            date: 1389390694,
+            mag: 1,
+        }, {
+            id: 1,
+            author: 'Séb',
+            title: 'News n°1',
+            content: 'Mon super text, avec du html: <img src="http://www.fricktal24.ch/typo3temp/pics/Ski-Aerni_Luca_01_55d138efee.jpg" /><a href="http://tooski.ch"></a>',
+            date: 1389390695,
+            mag: 0,
+        }, {
+            id: 2,
+            author: 'Séb',
+            title: 'News n°2',
+            content: 'Mon super text, avec du html: <a href="https://tooski.ch"><img src="http://skiweltcup.tv/wp-content/themes/tvsportnews/images/09-aerni002-klein-swiss-ski.jpg" /></a>',
+            date: 1389390696,
+            mag: 0,
+        }, {
+            id: 0,
+            author: 'Séb',
+            title: 'News n°0',
+            content: 'Mon super text, avec du html: <img src="http://www.google.ch/logos/doodles/2014/annette-von-droste-hulshoffs-217th-birthday-5701007384248320.2-hp.jpg" /><a href="http://tooski.ch"></a>',
+            date: 1389390694,
+            mag: 1,
+        }, {
+            id: 1,
+            author: 'Séb',
+            title: 'News n°1',
+            content: 'Mon super text, avec du html: <img src="http://www.fricktal24.ch/typo3temp/pics/Ski-Aerni_Luca_01_55d138efee.jpg" /><a href="http://tooski.ch"></a>',
+            date: 1389390695,
+            mag: 1,
+        }, {
+            id: 2,
+            author: 'Séb',
+            title: 'News n°2',
+            content: 'Mon super text, avec du html: <a href="https://tooski.ch"><img src="http://skiweltcup.tv/wp-content/themes/tvsportnews/images/09-aerni002-klein-swiss-ski.jpg" /></a>',
+            date: 1389390696,
+            mag: 0,
+        }, ];
 
-    var meaningOfLife = 42;
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+        return {
+            getNews: function(id) {
+                if (id) {
+                    return news[id];
+                }
+                else {
+                    return news;
+                }
+            },
+
+        };
+    });
