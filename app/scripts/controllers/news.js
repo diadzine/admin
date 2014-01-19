43441ea5da6ac6ad04f0ce179681ba64bbab408a
seba-1511 <seba-1511@hotmail.com>
2014-01-19 23:35:24 +0100

    Changes to the configuration of news page.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -2,5 +2,17 @@
 
 angular.module('adminApp')
     .controller('NewsCtrl', function($scope) {
+        $scope.tinymceOptions = {
+            selector: "textarea",
+            theme: "modern",
+            plugins: [
+                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                "searchreplace wordcount visualblocks visualchars code fullscreen",
+                "insertdatetime media nonbreaking table contextmenu directionality",
+                "emoticons template paste textcolor"
+            ],
+            toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons",
+            image_advtab: true
+        };
 
     });
