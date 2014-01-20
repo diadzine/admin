6c733c2fd36a7c124f792c23b6022ffccac38af4
seba-1511 <seba-1511@hotmail.com>
2014-01-20 10:30:59 +0100

    Completed bindings with tinymce, date, title and id.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -2,7 +2,8 @@
 
 angular.module('adminApp')
     .controller('NewsCtrl', function($scope, News) {
-        var news = News.getNews();
+        var news = News.getNews(),
+            date = new Date().getTime() / 1000;
 
 
         $scope.news = news;
@@ -11,21 +12,31 @@ angular.module('adminApp')
             alert('News Sent');
             // should check date, title, tinymce
             // if id = -1 create new, else, modify
+
+
         };
 
         $scope.addNews = function() {
-            // Should reset tinymce, the date, title and id field. change text of button.
+            $scope.currentNews = {
+                date: date,
+            };
+            $scope.sendText = 'Créer News';
+            $scope.tinyMceContent = news.content;
         };
 
         $scope.modify = function(newsId) {
-            // TODO: implement change content of tinymce. (not working yet.)
             var news = News.getNews(newsId);
-            document.getElementById('newsSend').innerHTML =
-                'Envoyer modifications';
-            document.getElementById('newsTitle').value = news.title;
-            document.getElementById('tinymce').innerHTML = news.content;
-            document.getElementById('newsId').value = news.id;
+            $scope.currentNews = news;
+            $scope.sendText = 'Modifier News';
             $scope.tinyMceContent = news.content;
+            // TODO: implement change content of tinymce. (not working yet.)
+            // change doc.getEl with a scope variable. change also in view.
+        };
+
+        $scope.sendText = 'Créer News';
+
+        $scope.currentNews = {
+            date: date,
         };
 
         $scope.tinymceOptions = {
