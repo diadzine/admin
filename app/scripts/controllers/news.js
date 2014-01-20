e26278fc4e3603521f71dda82af5b4c3e64fd21f
seba-1511 <seba-1511@hotmail.com>
2014-01-20 12:30:25 +0100

    Minor.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -49,15 +49,15 @@ angular.module('adminApp')
         };
 
         $scope.tinymceOptions = {
-            selector: "textarea",
-            theme: "modern",
+            selector: 'textarea',
+            theme: 'modern',
             plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking table contextmenu directionality",
-                "emoticons template paste textcolor"
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking table contextmenu directionality',
+                'emoticons template paste textcolor'
             ],
-            toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons",
+            toolbar1: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons',
             image_advtab: true
         };
 
