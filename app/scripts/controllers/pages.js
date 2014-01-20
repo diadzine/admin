9397bfe5f11c1b68e6bd5910ddc0561af33982d9
seba-1511 <seba-1511@hotmail.com>
2014-01-20 15:06:23 +0100

    Greatly advanced the page admin.

--- app/scripts/controllers/pages.js
+++ app/scripts/controllers/pages.js
@@ -1,6 +1,53 @@
 'use strict';
 
 angular.module('adminApp')
-    .controller('PagesCtrl', function($scope) {
+    .controller('PagesCtrl', function($scope, Pages) {
+        var pages = Pages.getPages()
 
+        $scope.modify = function(id) {
+            debugger;
+            var page = Pages.getPages(id);
+            $scope.currentTitle = page.name;
+            $scope.activePage = id;
+        };
+
+        $scope.delete = function() {
+            var id = $scope.activePage,
+                conf = confirm('Voulez-vous vraiment supprimer la news n°' +
+                    id + ' ?');
+            if (conf) {
+                Pages.delete(id);
+                pages = Pages.getPages();
+                scope.modify(pages[0].id);
+            }
+            return;
+        };
+
+        $scope.save = function(id) {
+
+        };
+
+        $scope.addPage = function() {
+            var newPage = Pages.save();
+            $scope.modify(newPage.id);
+        };
+
+        $scope.currentPage = pages[0];
+
+        $scope.activePage = pages[0].id;
+
+        $scope.pages = pages;
+
+        $scope.tinymceOptions = {
+            selector: 'textarea',
+            theme: 'modern',
+            plugins: [
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking table contextmenu directionality',
+                'emoticons template paste textcolor'
+            ],
+            toolbar1: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons',
+            image_advtab: true
+        };
     });
--- app/scripts/services/Pages.js
+++ app/scripts/services/Pages.js
@@ -3,29 +3,80 @@
 angular.module('adminApp')
     .factory('Pages', function() {
         var pages = [{
-            id: 0
-            name: 'Test',
+            id: 0,
+            name: 'Publicité',
             content: '<p>Il n\'y a presque rien ici 1 ! </p>',
         }, {
-            id: 1
-            name: 'Test',
+            id: 1,
+            name: 'Staff',
             content: '<p>Il n\'y a presque rien ici 2 ! </p>',
         }, {
-            id: 2
-            name: 'Test',
+            id: 2,
+            name: 'Nounours',
             content: '<p>Il n\'y a presque rien ici 3 ! </p>',
         }, {
-            id: 4
-            name: 'Test',
+            id: 3,
+            name: 'Admin',
             content: '<p>Il n\'y a presque rien ici 4 ! </p>',
         }, {
-            id: 5
-            name: 'Test',
+            id: 4,
+            name: 'Jean de La Fontaine',
             content: '<p>Il n\'y a presque rien ici 5 ! </p>',
         }, ];
         return {
-            save: function() {
+            getPages: function(id) {
+                var iter;
+                if (angular.isNumber(id)) {
+                    for (iter = 0; iter < pages.length; iter++) {
+                        if (pages[iter].id === iter) {
+                            return pages[iter];
+                        }
+                    }
+                }
+                else {
+                    return pages;
+                }
+            },
 
+            delete: function(id) {
+                var iter;
+                for (iter = 0; iter < pages.length; iter++) {
+                    if (pages[iter].id === id) {
+                        pages.splice(iter, 1);
+                        return true;
+                    }
+                }
+                return false;
+            },
+
+            save: function(id, page) {
+                var iter;
+                if (id || angular.isNumber(id)) {
+                    for (iter = 0; iter < pages.length; iter++) {
+                        if (pages[iter].id === id) {
+                            pages[iter] = {
+                                id: id,
+                                name: page.name,
+                                content: page.content,
+                            };
+                            return pages[iter];
+                        }
+                    }
+                }
+                else {
+                    /*
+                     * TODO:
+                     * pages.length is not enough, you have to sync with server.
+                     */
+                    var id = pages.length,
+                        page = {
+                            id: id,
+                            name: 'Nouvelle page',
+                            content: '<p>Contenu de la nouvelle page.</p>',
+                        };
+                    pages.push(page);
+                    return page;
+                }
             },
 
         };
