791b7f1fda5e58656cf319bf170cdf24ce3f69b7
seba-1511 <seba-1511@hotmail.com>
2014-04-04 00:47:41 +1100

    Test written for Server factory.

--- test/spec/services/server.js
+++ test/spec/services/server.js
@@ -21,4 +21,41 @@ describe('Service: Server', function() {
             .toBe('string');
     });
 
+    it('should have a working error handler', function() {
+        expect(typeof Server.errorHandler)
+            .toBe('function');
+    });
+
+    it('should correctly process responses from the server', function() {
+        var response = [{
+            "pk": 1,
+            "model": "blogs.bloggers",
+            "fields": {
+                "ad": "",
+                "sponsors": "",
+                "linkResults": "",
+                "date": "2014-03-19T06:37:32Z",
+                "profilePic": "",
+                "biography": "Le premier blog de Tooski; sa mascotte !",
+                "name": "Ursi"
+            }
+        }],
+            processed = [{
+                "id": 1,
+                "ad": "",
+                "sponsors": "",
+                "linkResults": "",
+                "date": 1395211052000,
+                "profilePic": "",
+                "biography": "Le premier blog de Tooski; sa mascotte !",
+                "name": "Ursi"
+            }];
+
+        expect(typeof Server.processResponse)
+            .toBe('function');
+
+        expect(Server.processResponse(response))
+            .toEqual(processed);
+    });
+
 });
